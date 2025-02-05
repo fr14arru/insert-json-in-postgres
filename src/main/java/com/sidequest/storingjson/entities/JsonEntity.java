cef62afe3da1ad23c66588fa3a4ff86ac46783c8
Francisco Arce Ruiz <farceru@mercadona.es>
2025-02-05 11:16:18 +0100

    feat: test inserting in raw text column

--- src/main/java/com/sidequest/storingjson/entities/JsonEntity.java
+++ src/main/java/com/sidequest/storingjson/entities/JsonEntity.java
@@ -1,5 +1,6 @@
 package com.sidequest.storingjson.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,6 @@ public class JsonEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // TODO - column treatment
+    @Column(columnDefinition = "text")
     private String json;
 }
